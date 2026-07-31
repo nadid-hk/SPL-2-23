26acde979811677daf277b9083eafffd8f3d90cc
nadid07 <nadid320@gmail.com>
2026-08-01 00:37:22 +0600

    Update index.js
    removing the key

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,7 +4,7 @@ import './App.css';
 import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID || "883133758924-1ed2569dvrr3s9ltfkq6b7k4lncg0fvc.apps.googleusercontent.com";
+const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
