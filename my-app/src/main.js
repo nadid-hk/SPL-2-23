caec53fab2d770ce4ac77821cbade105996c5148
nadid07 <nadid320@gmail.com>
2026-08-01 00:38:13 +0600

    Update main.js
    removing the hard coded key big mistake from nadid side

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const CLIENT_ID = "883133758924-1ed2569dvrr3s9ltfkq6b7k4lncg0fvc.apps.googleusercontent.com";
+const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
